c767bb2608e87f938b0456c925a8edbf4d91882d
petercerhan <petercerhan@gmail.com>
2016-07-01 19:59:10 -0500

    Initial Commit

---
+++ ObjAnnuities/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjAnnuities
+//
+//  Created by Peter Cerhan on 7/1/16.
+//  Copyright © 2016 Peter Cerhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjAnnuities/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjAnnuities
+//
+//  Created by Peter Cerhan on 7/1/16.
+//  Copyright © 2016 Peter Cerhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
